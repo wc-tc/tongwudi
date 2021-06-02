eb2c590fa297e954ceb73bda9fca9e0c65c917f4
Yasuo <2225299233@qq.com>
2021-06-02 18:55:31 +0800

    用分而治之的D&C方法来用递归实现数组的快排

--- algorithm1/src/main/java/com/tongwudi/algorithm/Tool/TongWuDiTool.java
+++ algorithm1/src/main/java/com/tongwudi/algorithm/Tool/TongWuDiTool.java
@@ -20,4 +20,67 @@ public class TongWuDiTool {
         }
         return index;
     }
+
+    /**
+     * 分而久之思想实现快排
+     * @param a
+     * @return
+     */
+    public static Integer[] simpleDiGuiKuaiPai(Integer [] a){
+
+        if(a.length<2){
+            return a;
+        }else {
+            int pivot=a[0];
+            Integer[]b=new Integer[a.length];
+            int count=0;
+            for (int i = 1; i <a.length; i++) {
+                if(a[i]<=pivot){
+                    b[count]=a[i];
+                    count++;
+                }
+            }
+            Integer[] c=new Integer[count];
+            for (int i = 0; i <count; i++) {
+                c[i]=b[i];
+            }
+            count=0;
+            b=new Integer[a.length];
+            for (int i = 1; i <a.length; i++) {
+                if(a[i]>pivot){
+                    b[count]=a[i];
+                    count++;
+                }
+            }
+            Integer[] d=new Integer[count];
+            for (int i = 0; i <count; i++) {
+                d[i]=b[i];
+            }
+
+
+            return shuZuPingJie(simpleDiGuiKuaiPai(c),pivot,simpleDiGuiKuaiPai(d),a);
+        }
+
+    }
+
+    /**
+     * 拼接数组
+     * @param c  一个由基础准值划分的比他小的数组
+     * @param pivot 基准值
+     * @param d  一个由基准值划分的比他大的数组
+     * @param a  原数组
+     * @return
+     */
+    public static Integer[] shuZuPingJie(Integer [] c,int pivot,Integer d[],Integer [] a ){
+        Integer [] b=new Integer[a.length];
+        for (int i = 0; i <c.length; i++) {
+            b[i]=c[i];
+        }
+        b[c.length]=pivot;
+        int count=0;
+        for (int i = c.length+1; i <b.length; i++,count++) {
+            b[i]=d[count];
+        }
+        return b;
+    }
 }
--- algorithm1/src/main/java/com/tongwudi/algorithm/controller/SimplController.java
+++ algorithm1/src/main/java/com/tongwudi/algorithm/controller/SimplController.java
@@ -1,6 +1,7 @@
 package com.tongwudi.algorithm.controller;
 
 
+import com.tongwudi.algorithm.BeanDTO.kuaiPaiDTO;
 import com.tongwudi.algorithm.BeanDTO.shuZuAndOrderDTO;
 import com.tongwudi.algorithm.BeanDTO.xuanZheDTO;
 import com.tongwudi.algorithm.Tool.TongWuDiTool;
@@ -32,6 +33,7 @@ public class SimplController {
     }
     @RequestMapping("/xuanZhe")
     public  Integer[] xuanZhe(@RequestBody xuanZheDTO xuanZheDTO){
+        long startTime=System.nanoTime(); //获取开始时间
         if(xuanZheDTO==null){
             return null;
         }
@@ -44,7 +46,24 @@ public class SimplController {
             newArr[i]=xuanZheDTO.getArr()[i1];
             xuanZheDTO.getArr()[i1]=Integer.MAX_VALUE;
         }
+        long endTime=System.nanoTime(); //获取结束时间
+        System.out.println("程序运行时间： "+(endTime-startTime)+"ns");
         return newArr;
+    }
+    /**
+     * 用分而治之D&C的思想去用java实现递归数组快排
+     *
+     */
+    @RequestMapping("/kuaiPai")
+    public  Integer[] kuaiPai(@RequestBody  kuaiPaiDTO kuaiPaiDTO){
+        long startTime=System.nanoTime(); //获取开始时间
+        if(kuaiPaiDTO==null){
+            return null;
+        }
+        Integer[] integers = TongWuDiTool.simpleDiGuiKuaiPai(kuaiPaiDTO.getArr());
+        long endTime=System.nanoTime(); //获取结束时间
+        System.out.println("程序运行时间： "+(endTime-startTime)+"ns");
+        return integers;
     }
 
 }
